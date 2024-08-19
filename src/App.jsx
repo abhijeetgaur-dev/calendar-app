61e30fab7b7e3d7610e3284803ddb034efe7d744
Abhijeet Gaur <abhijeetgaur.dev@gmail.com>
2024-08-19 20:28:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import CalendarApp from './Components/CalendarApp'
+import Footer from './Components/Footer'
+import './Components/CalendarApp.css'
+import './Components/Footer.css'
+
+const App = () => {
+  return (
+    <div className="container">
+      <CalendarApp/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
