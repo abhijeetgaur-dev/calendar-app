56640a6fd72a741f08d03cf74cc7fc2b9628d7a9
Abhijeet Gaur <abhijeetgaur.dev@gmail.com>
2024-08-21 09:37:53 +0530

    footer update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,12 @@
 const Footer = () => {
   return (
-    <div className="footer-box">
+    <div className="align-items">
         <div className="div footer-content">
         Created by Abhijeet Gaur
         <div className="footer-links">
         <a href="https://www.linkedin.com/in/abhijeet-gaur-848754300/">LinkedIn</a> ·
         <a href="https://github.com/abhijeetgaur-dev">GitHub</a> · 
-        <a href="mailto:abhijeetgaur777@gmail.com">Contact</a> · 
+        <a href="mailto:abhijeetgaur.dev@gmail.com">Contact</a> · 
         <a href="https://drive.google.com/drive/folders/1I5mWLN7M5ukHls3ULKItJ3q2M0ejjlUc?usp=drive_link">Resume</a>
         </div>
     </div>
